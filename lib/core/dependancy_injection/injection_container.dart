76740fd3b6a8d09a559fd100d7236500619ba6e7
Navas Shareef <navasshareef188@gmail.com>
2026-03-06 04:52:20 +0530

    added dependancy injection using get_it

---
+++ lib/core/dependancy_injection/injection_container.dart
@@ -0,0 +1,42 @@
+import 'package:get_it/get_it.dart';
+import 'package:guardian_route/features/tracking_dashboard/data/data_sources/location_db_datasource.dart';
+import 'package:guardian_route/features/tracking_dashboard/data/data_sources/locationtracking_datasource.dart';
+import 'package:guardian_route/features/tracking_dashboard/data/repositories/location_repo_imp.dart';
+import 'package:guardian_route/features/tracking_dashboard/domain/repositories/location_repository.dart';
+import 'package:guardian_route/features/tracking_dashboard/domain/usecases/check_tracking_status.dart';
+import 'package:guardian_route/features/tracking_dashboard/domain/usecases/start_tracking.dart';
+import 'package:guardian_route/features/tracking_dashboard/domain/usecases/stop_tracking.dart';
+import 'package:guardian_route/features/tracking_dashboard/domain/usecases/tracking_status_stream.dart';
+import 'package:guardian_route/features/tracking_dashboard/presentation/bloc/location_tracking_bloc/location_bloc.dart';
+
+final sl = GetIt.instance;
+
+Future init() async {
+  sl.registerFactory(
+    () => LocationBloc(
+      startTracking: sl(),
+      stopTracking: sl(),
+      checkTrackingStatus: sl(),
+      trackingStatusStream: sl(),
+    ),
+  );
+
+  sl.registerLazySingleton(() => StartTracking(sl()));
+  sl.registerLazySingleton(() => StopTracking(sl()));
+  sl.registerLazySingleton(() => CheckTrackingStatus(sl()));
+  sl.registerLazySingleton(() => TrackingStatusStream(sl()));
+
+  sl.registerLazySingleton<LocationTrackingDataSource>(
+    () => LocationTrackingDataSourceImpl(),
+  );
+  sl.registerLazySingleton<LocationLocalDataSource>(
+    () => LocationLocalDataSourceImpl(),
+  );
+
+  sl.registerLazySingleton<LocationRepository>(
+    () => LocationRepositoryImpl(
+      locationTrackingService: sl(),
+      localDataSource: sl(),
+    ),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:guardian_route/core/db/isar_service.dart';
+import 'package:guardian_route/core/dependancy_injection/injection_container.dart'
+    as di;
 import 'package:guardian_route/core/services/background_service_intializer.dart';
 import 'package:guardian_route/core/services/notification_service.dart';
 import 'package:guardian_route/core/theme/app_theme.dart';
-import 'package:guardian_route/features/tracking_dashboard/data/data_sources/location_db_datasource.dart';
-import 'package:guardian_route/features/tracking_dashboard/data/data_sources/locationtracking_datasource.dart';
-import 'package:guardian_route/features/tracking_dashboard/data/repositories/location_repo_imp.dart';
-import 'package:guardian_route/features/tracking_dashboard/domain/usecases/check_tracking_status.dart';
-import 'package:guardian_route/features/tracking_dashboard/domain/usecases/start_tracking.dart';
-import 'package:guardian_route/features/tracking_dashboard/domain/usecases/stop_tracking.dart';
-import 'package:guardian_route/features/tracking_dashboard/domain/usecases/tracking_status_stream.dart';
 import 'package:guardian_route/features/tracking_dashboard/presentation/bloc/history_bloc/history_bloc.dart';
 import 'package:guardian_route/features/tracking_dashboard/presentation/bloc/location_tracking_bloc/location_bloc.dart';
 import 'package:guardian_route/features/tracking_dashboard/presentation/bloc/location_tracking_bloc/location_event.dart';
 import 'package:guardian_route/routes/app_router.dart';
 
-/// Create single instances
-final dataSource = LocationTrackingDataSourceImpl();
-final localDataSource = LocationLocalDataSourceImpl();
-final repository = LocationRepositoryImpl(
-  locationTrackingService: dataSource,
-  localDataSource: localDataSource,
-);
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
   await IsarService.init();
   await BackgroundServiceInitializer.initialize();
   await NotificationService.requestNotificationPermission();
@@ -41,14 +29,8 @@ class MyApp extends StatelessWidget {
     return MultiBlocProvider(
       providers: [
         BlocProvider(
-          create: (_) => LocationBloc(
-            startTracking: StartTracking(repository),
-            stopTracking: StopTracking(repository),
-            checkTrackingStatus: CheckTrackingStatus(repository),
-            trackingStatusStream: TrackingStatusStream(repository),
-          )..add(CheckTrackingStatusEvent()),
+          create: (_) => di.sl<LocationBloc>()..add(CheckTrackingStatusEvent()),
         ),
-
         BlocProvider(create: (_) => HistoryBloc(), lazy: false),
       ],
       child: MaterialApp(
